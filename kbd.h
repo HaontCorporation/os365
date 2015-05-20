3f3b1edd55853eca9dfc1aaede6764840cbf9fe0
catnikita255 <kras-ivanova@yandex.ru>
2015-05-20 18:33:13 +0300

    Update kbd.h

--- kbd.h
+++ kbd.h
@@ -1,5 +1,7 @@
 #ifndef __KBD_H
 #define __KBD_H
+#include "io.h"
+#include "graphics.h"
 void waitForKey(int key)
 {
 	char c;
@@ -14,5 +16,62 @@ void waitForKey(int key)
 	}
 	while(c!=key);
 	return;
+}
+char* gets(word x,word y)
+{
+	char c;
+	char* ret;
+	int i = 0;
+	bool caps = false;
+	do
+	{
+		if(inb(0x60)!=c)
+		{
+			c = inb(0x60);
+			if(c == 0x0E)
+			{
+				if(x != 0)
+				{
+					if(y != 0)
+					{
+						x = x - 8;
+						vgaPutchar(' ',x,y,MAGENTA,LIGHT_GREY);
+					}
+				}
+				else
+				{
+					y = y - 8;
+					x = 639;
+				}
+				goto skip2;
+			}
+			if(c == 0x3A)
+			{
+				if(caps == false)
+				caps = true;
+				else
+				caps = false;
+				goto skip2;
+			}
+			if(c>0)
+			{
+			if(caps == false)
+			{
+			ret[i] = scancode[c+1];
+			vgaPutchar(scancode[c+1],x,y,MAGENTA,LIGHT_GREY);
+			}
+			else
+			{
+			ret[i] = scancode[c+1+90];
+			vgaPutchar(scancode[c+1+90],x,y,MAGENTA,LIGHT_GREY);
+			}
+			i++; x = x + 8;
+			}
+		}
+		skip2:
+		caps = caps;
+	}
+	while(c!=28);
+	return ret;
 }
 #endif
